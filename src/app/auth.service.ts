70f38da1acc6bb139803f67e92f808a56c954846
Bernardo Pineda <_@bernardopineda.mx>
2024-06-28 18:22:15 -0700

    Added subject and behavior to emit message; added pocketbase and environment variables

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Subject, BehaviorSubject } from 'rxjs';
+import PocketBase from 'pocketbase';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  pb: PocketBase;
 
-  constructor() { }
+  constructor() { 
+    this.pb = new PocketBase(environment.baseUrl);
+  }
 }
