d614e43f0d65e7407cbd696ea4baf1e504877948
Bernardo Pineda <_@bernardopineda.mx>
2024-06-30 11:42:37 -0700

    Imported AuthGuard on routes and implemented canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './auth-guard.service';
 
 const routes: Routes = [
-  { path:  '', component:  HomeComponent},
+  { path:  '', component:  HomeComponent, canActivate: [AuthGuardService]},
   { path:  'login', component:  LoginComponent},
 ];
 
