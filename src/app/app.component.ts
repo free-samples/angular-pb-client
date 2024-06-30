2f5b93a73fc5e7d1d11067a71e81a1d91f77e50a
Bernardo Pineda <_@bernardopineda.mx>
2024-06-30 11:12:39 -0700

    Importing AuthService, Router and subscription; listening to emmitted event

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-app';
+  isLoggedIn = false;
+  buttonClickedSubscription: Subscription;
+
+  constructor(private authService: AuthService, private router: Router){
+
+  }
+
+  ngOnInit(): void {
+    this.buttonClickedSubscription = this.authService.emitLogin().subscribe( ()=> alert(123) );
+  }
+
 }
