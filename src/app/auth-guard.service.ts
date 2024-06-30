435b219a5b5c31d150441c3796eace981f62f1b1
Bernardo Pineda <_@bernardopineda.mx>
2024-06-30 11:43:17 -0700

    Added AuthService on AuthGuard service and implemented CanActivate

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
+
+  canActivate(): boolean {
+    if (this.authService.isSessionValid()) {
+      return true; // Allow access if the user is authenticated
+    } else {
+      this.router.navigate(['login']); // Redirect to login if not authenticated
+      return false; // Prevent access to the route
+    }
+  }
 }
